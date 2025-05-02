59fbfb563e3d74fd93d34ac4cf78e3c6520ae337
ahjadi <alihussainaljadi@gmail.com>
2025-05-02 14:25:27 +0300

    setup(project): initializing spring and set structure

---
+++ src/test/kotlin/com/cornerstone/bank_pots/BankPotsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cornerstone.bank_pots
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankPotsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
