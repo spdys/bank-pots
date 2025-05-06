f2be1f5a28453541bc28942e0abf66b2e7f12ee6
ahjadi <alihussainaljadi@gmail.com>
2025-05-06 09:45:35 +0300

    feat(exception handling): handle user controller password invalid exception

--- auth-service/src/main/kotlin/authentication/controller/UserController.kt
+++ auth-service/src/main/kotlin/authentication/controller/UserController.kt
@@ -4,6 +4,7 @@ import authentication.dto.UserCreationRequest
 import authentication.entity.UserEntity
 import authentication.repository.UserRepository
 import authentication.service.UserService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,8 +16,13 @@ import org.springframework.web.bind.annotation.RestController
 class UserController(val userRepository: UserRepository, private val userService: UserService) {
 
     @PostMapping("/register")
-    fun registerUser(@RequestBody user: UserCreationRequest){
-        userService.createUser(user)
+    fun registerUser(@RequestBody user: UserCreationRequest) : ResponseEntity<Any> {
+
+        return try {
+            ResponseEntity.ok().body(userService.createUser(user))
+        } catch (e: IllegalArgumentException){
+            ResponseEntity.badRequest().body(mapOf("error" to e.message))
+        }
     }
 
 
