6795da2c98ce2e6763f53f5989b2c18ab4387493
ahjadi <alihussainaljadi@gmail.com>
2025-05-10 09:47:42 +0300

    replaced println with logger

--- auth-service/src/main/kotlin/authentication/security/jwt/JwtAuthenticationFilter.kt
+++ auth-service/src/main/kotlin/authentication/security/jwt/JwtAuthenticationFilter.kt
@@ -5,6 +5,7 @@ import authentication.security.UserPrincipal
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
@@ -20,13 +21,13 @@ class JwtAuthenticationFilter(
     private val jwtService: JwtService,
     private val userDetailsService: CustomUserDetailsService,
 ) : OncePerRequestFilter() {
+    private val log = LoggerFactory.getLogger(JwtAuthenticationFilter::class.java)
 
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        println("JwtAuthenticationFilter triggered")
         val authHeader = request.getHeader("Authorization")
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response)
@@ -35,16 +36,11 @@ class JwtAuthenticationFilter(
 
         val token = authHeader.substring(7)
         val username = jwtService.extractUsername(token)
-        println("Extracted username: $username")
         if (username != null && SecurityContextHolder.getContext().authentication == null) {
-            println("Username extracted and no authentication in context")
             if (jwtService.isTokenValid(token, username)) {
-                println("Token is valid for username: $username")
                 val userDetails = userDetailsService.loadUserByUsername(username)
                 val roleFromJwt = jwtService.extractRole(token) // Extract directly from token
-                println("Role extracted from JWT: $roleFromJwt")
                 val authorities = listOf(SimpleGrantedAuthority("ROLE_$roleFromJwt")) // Add correct role prefix
-                println("Created authorities: $authorities")
 
 
                 val userPrincipal = if (userDetails is UserPrincipal) {
@@ -55,21 +51,17 @@ class JwtAuthenticationFilter(
                         token = token,
                         password = userDetails.password,
                     )
-                    println("Created UserPrincipal with role: ${principal.authorities}")
                     principal
                 } else {
-                    println("Using original userDetails")
                     userDetails
                 }
 
                 val authToken = UsernamePasswordAuthenticationToken(userPrincipal, null, authorities)
                 authToken.details = WebAuthenticationDetailsSource().buildDetails(request)
                 SecurityContextHolder.getContext().authentication = authToken
-                println("Set authentication in SecurityContext with authorities: ${authToken.authorities}")
 
             } else
-                println("Token is NOT valid for username: $username")
-
+                logger.error("Token is NOT valid for username: $username")
         }
 
         filterChain.doFilter(request, response)
